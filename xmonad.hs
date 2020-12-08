07dba73b12917416e6814752359ac854b3f61875
jfh <jfh@imijmi.eu>
2020-12-08 20:03:27 +0200

    Add changes for xmonad (layouts). Create branch.

--- xmonad.hs
+++ xmonad.hs
@@ -154,14 +154,14 @@ myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
 -- restarting (with 'mod-q') to reset your layout state to the new
 -- defaults, as xmonad preserves your old layout settings by default.
 myLayout = spacingRaw True (Border 0 0 0 0) True (Border 10 6 6 6) True $
-           tiled ||| Full ||| spiral (6/7)  
+           tiled ||| Full ||| spiral (7/8) 
   where
      -- default tiling algorithm partitions the screen into two panes
      tiled   = Tall nmaster delta ratio
      -- The default number of windows in the master pane
      nmaster = 1
      -- Default proportion of screen occupied by master pane
-     ratio   = 1/2
+     ratio   = 4/6
      -- Percent of screen to increment by when resizing panes
      delta   = 3/100
 
